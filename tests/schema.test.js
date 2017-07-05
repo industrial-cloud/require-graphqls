03245ad9acac7fec27a7d1886ef65f8fa7960523
avalla <valla.andrea@gmail.com>
2017-07-05 16:14:19 +0200

    minor

--- tests/schema.test.js
+++ tests/schema.test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+
 const expect = chai.expect;
 
 require('../index');
@@ -7,7 +8,6 @@ describe('# Read graphql schema', function() {
   it('Read file .graphqls should extract a string', function(done) {
     const schema = require('./schema.graphqls');
     expect(schema).to.be.a('string');
-    console.log(schema);
-    done()
+    done();
   });
 });
